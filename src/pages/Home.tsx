b82c9e25b6af7e54b6e488562b1e60ca9330842e
FahadBinAbdullah7 <78896691+FahadBinAbdullah7@users.noreply.github.com>
2025-10-25 14:58:03 +0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -669,7 +669,7 @@ const Home = () => {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 IELTS Ace. All rights reserved.</p>
+            <p>&copy; 2025 IELTS Ace . All rights reserved.</p>
           </div>
         </div>
       </footer>
